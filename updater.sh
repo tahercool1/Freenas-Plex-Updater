5b82d07984a818878a894805bd612311d533c784
Taherm789 <12758805+Taherm789@users.noreply.github.com>
2019-05-27 12:35:05 +0530

    Create updater.sh
    First commit
    Plex version 1.15.6.1079

---
+++ updater.sh
@@ -0,0 +1,9 @@
+mv plexmediaserver plexmediaserver-old-$(date -d "today" +"%Y%m%d%H%M")
+wget https://downloads.plex.tv/plex-media-server-new/1.15.6.1079-78232c603/freebsd/PlexMediaServer-1.15.6.1079-78232c603-FreeBSD-amd64.tar.bz2
+tar -xvf PlexMediaServer-1.15.6.1079-78232c603-FreeBSD-amd64.tar.bz2
+rm PlexMediaServer-1.15.6.1079-78232c603-FreeBSD-amd64.tar.bz2
+mv PlexMediaServer-1.15.6.1079-78232c603/ plexmediaserver
+chown -R root:wheel plexmediaserver
+chmod 775 plexmediaserver/*
+ln ./plexmediaserver/Plex\ Media\ Server ./plexmediaserver/Plex_Media_Server
+chmod -h 775 ./plexmediaserver/Plex_Media_Server
