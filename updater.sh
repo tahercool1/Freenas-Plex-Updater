cb4ba2d27edcfbdf4648e01c4a9be031424e6d5c
Taherm789 <12758805+Taherm789@users.noreply.github.com>
2019-08-01 00:09:53 +0530

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,11 +1,11 @@
 #!/bin/csh
 
 cd /usr/local/share/
-wget https://downloads.plex.tv/plex-media-server-new/1.16.1.1291-158e5b199/freebsd/PlexMediaServer-1.16.1.1291-158e5b199-FreeBSD-amd64.tar.bz2
-tar -xvf PlexMediaServer-1.16.1.1291-158e5b199-FreeBSD-amd64.tar.bz2
-rm PlexMediaServer-1.16.1.1291-158e5b199-FreeBSD-amd64.tar.bz2
+wget https://downloads.plex.tv/plex-media-server-new/1.16.3.1402-22929c8a2/freebsd/PlexMediaServer-1.16.3.1402-22929c8a2-FreeBSD-amd64.tar.bz2
+tar -xvf PlexMediaServer-1.16.3.1402-22929c8a2-FreeBSD-amd64.tar.bz2
+rm PlexMediaServer-1.16.3.1402-22929c8a2-FreeBSD-amd64.tar.bz2
 mv plexmediaserver plexmediaserver-old-`date +%Y-%m-%d.%H:%M:%S`
-mv PlexMediaServer-1.16.1.1291-158e5b199/ plexmediaserver
+mv PlexMediaServer-1.16.3.1402-22929c8a2/ plexmediaserver
 chown -R root:wheel plexmediaserver
 chmod 775 plexmediaserver/*
 ln ./plexmediaserver/Plex\ Media\ Server ./plexmediaserver/Plex_Media_Server
